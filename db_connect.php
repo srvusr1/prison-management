ed8b0d3728122f867dfd41d3a0515ee798a5cd97
srvusr1 <116091538+srvusr1@users.noreply.github.com>
2022-10-18 20:55:43 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -3,6 +3,8 @@
     $conn = mysqli_connect('localhost','sourav','sourav123','prison');
 
     //check connection
+
+
     if(!$conn){
         echo 'Connection error : ' . mysqli_connect_error();
     }
